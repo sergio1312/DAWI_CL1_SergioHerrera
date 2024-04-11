306aaaf09cd18d35f47aae9505927f47b7219c92
Sergio <serham_95@hotmail.com>
2024-04-11 17:43:49 -0500

    evaluacion CL1 DAW1

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_SergioHerrera/DawiCl1SergioHerreraApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_SergioHerrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1SergioHerreraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1SergioHerreraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWI_CL1_SergioHerrera/DawiCl1SergioHerreraApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_SergioHerrera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1SergioHerreraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
